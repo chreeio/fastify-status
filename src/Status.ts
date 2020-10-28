23b5b132b200d06cdeac8d10eb263c14aac238a8
Attila Bagossy <bagossyattila@outlook.com>
2020-10-28 17:30:13 +0100

    docs: documentation for Status

--- src/Status.ts
+++ src/Status.ts
@@ -1,5 +1,27 @@
+/**
+ * Enumeration of possible status values for service health.
+ */
 export enum Status {
+  /**
+   * The service is up and running correctly, satisfying the functional and
+   * non-functional requirements.
+   */
   OK = 'ok',
+
+  /**
+   * The service might not be able to meet every requirement, but is able to fulfill its role.
+   *
+   * For example, if every database query is required to finish under 250 milliseconds, then
+   * we can set the status of the database connection to degraded if recent queries take longer
+   * than that.
+   */
   DEGRADED = 'degraded',
+
+  /**
+   * The service is not able to continue its job.
+   *
+   * For example, if the underlying database has gone offline, then we can set the status of
+   * the database connection to failing.
+   */
   FAILING = 'failing',
 }
